9ac17b27fd3e9e15e9eba226e8a2a5694e68b148
aselapasinduD <asela.pasindu.dias17@gmail.com>
2025-05-16 21:07:09 +0530

    Stage: Development | Area: Frontend-Side-Navbar | Work: Fixed not showing button title and unclicked white color icon not change bug fixed.

--- frontend/src/components/elements/sideNavbarButton.tsx
+++ frontend/src/components/elements/sideNavbarButton.tsx
@@ -1,4 +1,6 @@
-import React, {useState} from "react";
+import React from "react";
+
+import toCascadeCase from "../../@common/toCascadeCase";
 
 interface ButtonProps {
     icon: React.ReactNode
@@ -7,11 +9,19 @@ interface ButtonProps {
     setIsChecked: React.Dispatch<React.SetStateAction<string>>;
 }
 
+/**
+ * Side Navigation Bar for SPA
+ * 
+ * @param icon - Icon for the navigation side bar
+ * @param btnId - Unique Button ID
+ * @param isChecked - Current Selected side bar button ID
+ * @param setIsChecked - function to set the current selected side bar button ID
+ * @returns JSX element
+ */
 const SideNavbarButton: React.FC<ButtonProps> = ({btnId, icon, isChecked, setIsChecked}) => {
     const _isChecked = isChecked === btnId
-
     return(
-        <button type="button" className={`p-2 w-fit h-fit rounded-lg [&>svg]:text-[var(${_isChecked ? "--side-navbar-selected-btn-background" : "--side-navbar-default-icon-color" })] ${_isChecked ? "bg-[var(--side-navbar-selected-btn-background)]" : ""}`} onClick={() => setIsChecked(btnId)}>
+        <button type="button" title={toCascadeCase(btnId)} className={`p-2 w-fit h-fit rounded-lg [&>svg]:text-[var(${_isChecked ? "--side-navbar-background" : "--side-navbar-default-btn-icon-color" })] ${_isChecked ? "bg-[var(--side-navbar-selected-btn-background)]" : ""}`} onClick={() => setIsChecked(btnId)}>
             {icon}
         </button>
     )
