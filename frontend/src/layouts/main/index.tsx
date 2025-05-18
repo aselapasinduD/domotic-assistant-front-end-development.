24d8125d96ad4787310ec5df350e796b8e370665
aselapasinduD <asela.pasindu.dias17@gmail.com>
2025-05-18 14:50:42 +0530

    Stage: Development | Area: Frontend-Login-Page | Work: Updated the main layout file to check authentication token and return login page

--- frontend/src/layouts/main/index.tsx
+++ frontend/src/layouts/main/index.tsx
@@ -1,18 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import UserModel from "../../models/userModel";
 
 import Header from "../../components/widgets/header";
 import SideNavbar from "../../components/widgets/sideNavbar";
 
+import Login from "../../pages/login";
+
 interface PropType{
     children: React.ReactNode
 }
-
+/**
+ * Main Layout of the SPA
+ * 
+ * @param children - Interface of each tabs
+ * @returns JSX element
+ */
 function Layout({children}: PropType){
+    const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+
+    useEffect(() => {
+        setIsAuthenticated(UserModel.checkUserCredential())
+        setIsLoading(true)
+    }, [])
+
+    if (!isAuthenticated && isLoading) {
+        return <Login />;
+    }
+
     return(
-        <main className="min-h-screen bg-[var(--color-primary)]">
+        <main className="min-h-screen bg-[var(--primary-background)]">
             <Header />
             <SideNavbar />
-            <div style={{gridArea: "interface-container"}}>
+            <div className="relative" style={{gridArea: "interface-container"}}>
                 {children}
             </div>
         </main>
